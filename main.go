7a320832614173aaa5f0d2e8b46cb23cf79b005f
Luis Felipe <luisfelipegodoi@live.com>
2020-12-11 16:00:52 -0300

    feat: added initial features

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"os/exec"
+)
+
 func main() {
 
+	output, err := exec.Command("./youtubedr", "download", "Sv6dMFF_yts").Output()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(string(output))
 }
