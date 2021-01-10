11e748ddfbfa3c533d6540d8855413c4f7b262e7
Luis Felipe <godoiluisfelipe@gmail.com>
2021-01-10 18:44:26 -0300

    feat: added MoveFile method for move file.

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"os/exec"
 )
+
+func init() {
+	fmt.Println("Initializing Process..")
+}
 
 func main() {
 
@@ -12,5 +18,39 @@ func main() {
 		fmt.Println(err.Error())
 	}
 
+	// Move file to videos folder
+
+	// Printing Process
 	fmt.Println(string(output))
+
+	defer fmt.Println("Process Finalized!!!")
+}
+
+func MoveFile(sourcePath, destinationPath string) error {
+
+	inputFile, err := os.Open(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Couldn't open source file: %s", err)
+	}
+
+	outputFile, err := os.Create(destinationPath)
+	if err != nil {
+		inputFile.Close()
+		return fmt.Errorf("Couldn't open destination file: %s", err)
+	}
+
+	defer outputFile.Close()
+	_, err = io.Copy(outputFile, inputFile)
+	inputFile.Close()
+	if err != nil {
+		return fmt.Errorf("Writing to output file failed: %s", err)
+	}
+
+	// The copy was successful, so now delete the original file
+	err = os.Remove(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Failed removing original file: %s", err)
+	}
+
+	return nil
 }
